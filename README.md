fff9d77bed5d9427414c1b7973c3cc36a8a25cf0
Brian Tran <70680670+brian-trann@users.noreply.github.com>
2021-10-12 18:09:24 -0700

    refactor, add functionality, add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# REMAX tsconfig (and prettier)
+
+A tool to setup `tsconfig.json`, `prettierrc.json`, and `.prettierignore`
+
+## Usage
+```
+npx remax-tsconfig
+```
+
+* Use arrow keys to select Node configuration. 
+* Use `Y/n` to select `yes` or `no` for `prettierrc.json`, and `.prettierignore` files. If you click `enter`, it will default to creating and **overwriting** files.
+
+## To Do
+- [ ] Add logic to prevent overwriting files
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const inquirer = require("inquirer");
 const path = require("path");
-const { writeFile, readdir, readFile, mkdir } = require("fs").promises;
+const { writeFile, readdir, readFile } = require("fs").promises;
 
 const tsconfigFolderPath = path.resolve(__dirname, "tsconfig");
 const prettierFolderPath = path.resolve(__dirname, "prettier");
@@ -9,21 +9,34 @@ const prettierFolderPath = path.resolve(__dirname, "prettier");
 (async () => {
   const tsconfigFiles = await readdir(tsconfigFolderPath).catch(console.log);
   const tsconfigTypes = {};
-  
+
   for (let file of tsconfigFiles) {
     const nodeType = file.split(".")[1];
     tsconfigTypes[nodeType] = path.join(tsconfigFolderPath, file);
   }
-  
-  const { nodeType } = await inquirer.prompt([
+
+  const { nodeType, makePrettierIgnore, makePrettierRc } = await inquirer.prompt([
     {
       type: "list",
-      message: "Pick the Node type",
+      message: "Pick the Node type:",
       name: "nodeType",
       choices: ["node-default", "node-recommended", "node-14", "node-16"],
     },
+    {
+      type: "confirm",
+      message: "Do you want a .prettierignore?",
+      name: "makePrettierIgnore",
+      default: true,
+    },
+    {
+      type: "confirm",
+      message: "Do you want a prettierrc.json?",
+      name: "makePrettierRc",
+      default: true,
+    },
   ]);
   
+
   const tsconfigData = await readFile(tsconfigTypes[nodeType]).catch(console.log);
   const tsconfigPath = path.join(process.cwd(), "tsconfig.json");
 
@@ -31,23 +44,28 @@ const prettierFolderPath = path.resolve(__dirname, "prettier");
     console.log(e);
     process.exit(1);
   });
-  console.log("tsconfig.json created");
+  console.log("\ntsconfig.json created");
 
-  const prettierIgnoreData = await readFile(path.join(prettierFolderPath, "prettierignore")).catch(console.log);
-  const prettierIgnorePath = path.join(process.cwd(), ".prettierignore");
+  if (makePrettierIgnore) {
+    const prettierIgnoreData = await readFile(path.join(prettierFolderPath, "prettierignore")).catch(console.log);
+    const prettierIgnorePath = path.join(process.cwd(), ".prettierignore");
 
-  await writeFile(prettierIgnorePath, prettierIgnoreData).catch((e) => {
-    console.log(e);
-    process.exit(1);
-  });
-  console.log(".prettierignore created");
+    await writeFile(prettierIgnorePath, prettierIgnoreData).catch((e) => {
+      console.log(e);
+      process.exit(1);
+    });
+    console.log(".prettierignore created");
+  }
 
-  const prettierRcData = await readFile(path.join(prettierFolderPath, "prettierrc.json")).catch(console.log);
-  const prettierRcPath = path.join(process.cwd(), "prettierrc.json");
+  if (makePrettierRc) {
+    const prettierRcData = await readFile(path.join(prettierFolderPath, "prettierrc.json")).catch(console.log);
+    const prettierRcPath = path.join(process.cwd(), "prettierrc.json");
 
-  await writeFile(prettierRcPath, prettierRcData).catch((e) => {
-    console.log(e);
-    process.exit(1);
-  });
-  console.log("prettier.json created\n");
+    await writeFile(prettierRcPath, prettierRcData).catch((e) => {
+      console.log(e);
+      process.exit(1);
+    });
+    console.log("prettier.json created");
+  }
+  console.log("\n");
 })();
